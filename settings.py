7e15a1647e1ec60d2844b7d35b91eacf77883b5f
Владимир <skyfallagain009@gmail.com>
2021-12-11 10:49:08 +0300

    параметры экрана

--- settings.py
+++ settings.py
@@ -1,1 +1,9 @@
 class Settings():
+    """клас для хранения всех настроек игры"""
+
+    def __init__(self):
+        """инициализирует все настройки иггры"""
+        #  параметры экрана
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0, 0, 250)
