445873c9c78a8312a2c303ace28299662b4e2f1e
Владимир <skyfallagain009@gmail.com>
2021-12-11 11:41:39 +0300

    + загрузка изображения + положение попугая

--- popugai.py
+++ popugai.py
@@ -9,5 +9,11 @@ class Poougai():
         self.screen = popugai_game.screen  # для обращения во всех модулях класса
         self.screen_rect = popugai_game.screen.get_rect()  # размещение в нужной позиции
 
+        # загрузка изображения
+        self.image = pygame.image.load('images/popug.bmp')  # загрузка изображения попугая
+        self.rect = self.image.get_rect()  # размещение на поверхности окна изображения попугая
+        # каждый новый попугай появляется у нижнего края платформы
+        self.rect.midbottom = self.screen_rect.midbottom
+
 
 
