554dd04efde21d62bd42973535917701d24eb551
Владимир <skyfallagain009@gmail.com>
2021-12-11 10:36:34 +0300

    установка размера экрана и названия окна

--- init_practica.py
+++ init_practica.py
@@ -5,7 +5,12 @@ import sys
 class InitPractica():
     """класс для упраления русурсами и поведением игры"""
 
-
     def __init__(self):
         """инициализирует угру и создает игровые ресурсы"""
         pygame.init()
+
+        self.screen = pygame.display.set_mode((1200, 800))  # размер экрана
+        pygame.display.set_caption("Практика")  # верхня часть окна с названием
+
+
+
