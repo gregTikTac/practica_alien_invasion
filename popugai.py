7b36789a70b5e0e14e96d6a56831323e10f4ead1
Владимир <skyfallagain009@gmail.com>
2021-12-14 17:43:38 +0300

    + флаг перемещения + функц update которая обновлет позицю попугая

--- popugai.py
+++ popugai.py
@@ -15,6 +15,13 @@ class Popugai():
         # каждый новый попугай появляется у нижнего края платформы
         self.rect.midbottom = self.screen_rect.midbottom
 
+        self.moving_right = False  # флаг перемещения
+
+    def update(self):
+        """Обновление позиции попугая"""
+        if self.moving_right:
+            self.rect.x += 1
+
     def blitme(self):  # метод, который выводит изображения на экран в прзиции self.rect
         """Рисует попугая в текущей позиции"""
         self.screen.blit(self.image, self.rect)
